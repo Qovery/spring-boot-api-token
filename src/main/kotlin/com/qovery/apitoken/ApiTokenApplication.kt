673557309d938aa363ca343ce1ebcffa359f8e2d
Melvin Zottola <melvin.zottola@gmail.com>
2022-04-02 14:26:42 +0200

    Bootstrap project

---
+++ src/main/kotlin/com/qovery/apitoken/ApiTokenApplication.kt
@@ -0,0 +1,11 @@
+package com.qovery.apitoken
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiTokenApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiTokenApplication>(*args)
+}
